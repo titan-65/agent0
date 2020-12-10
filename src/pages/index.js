4c0f8a309bf44c3363b55df8ffef015197c6158e
Vantol Bennett <vvantol2000@icloud.com>
2020-12-10 01:41:43 -0500

    additon of icons and  sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,123 @@
-import { Banner, DynamicText, TextWrapper } from '../styles/indexStyles'
+import {
+  Banner,
+  DynamicText,
+  SectionTwo,
+  SectionThree,
+  SectionFour,
+  FLexBoxIndex,
+  TextWrapper,
 
-import { Link } from 'gatsby'
+} from "../styles/indexStyles"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faHeart, faCode, faGem, fas } from "@fortawesome/free-solid-svg-icons"
+import { Link } from "gatsby"
 import React from "react"
+
+library.add(faHeart, faCode, faGem, fas)
 
 export default function Home() {
   return (
-    <div style={{ position: "relative"}}>
-      <Banner>
-      </Banner>
-      <TextWrapper>
+    <>
+      <section style={{ position: "relative" }}>
+        <Banner></Banner>
+        <TextWrapper>
+          <div>
+            <h2>Secret Agent</h2>
+            <p>Games are not just for kids</p>
+            <Link to="/mode">Our work</Link>
+          </div>
+        </TextWrapper>
+        <DynamicText>Learn More</DynamicText>
+      </section>
+      <SectionTwo>
         <div>
-          <h2>Secret Agent</h2>
-          <p>Games are not just for kids</p>
-          <Link to="/mode">Our work</Link>
+          <h2>our Mission</h2>
+          <p>
+            Great programmers take on a new mission with every console.log()
+          </p>
+          <h5>- Vantol Bennett</h5>
         </div>
-      </TextWrapper>
-      <DynamicText>Learn More</DynamicText>
-    </div>
+        <span>
+          <FontAwesomeIcon
+            icon="gem"
+            color="#04f5c6"
+            size="6x"
+            style={{ marginRight: "3rem" }}
+            fixedWidth
+            border
+          />
+          <FontAwesomeIcon
+            icon="heart"
+            color="#00F0FF"
+            size="6x"
+            style={{ marginRight: "3rem" }}
+            fixedWidth
+            border
+          />
+          <FontAwesomeIcon
+            icon="code"
+            color="#73DBFD"
+            size="6x"
+            fixedWidth
+            border
+          />
+        </span>
+      </SectionTwo>
+      <SectionThree>
+        <FLexBoxIndex>
+          <div className="image">
+            <img src="pic01.jpg" alt="pic01" />
+          </div>
+          <div className="text_section3">
+            <h2>Code Development</h2>
+            <p>
+              Commodo reprehenderit qui sit esse magna enim sit esse dolore
+              proident eu deserunt nostrud. Officia ut id consectetur officia
+              sint minim sint sit occaecat duis ipsum sit. Ad voluptate tempor
+              nulla esse nisi. Culpa proident nulla irure reprehenderit aute
+              laboris aute.
+            </p>
+          </div>
+        </FLexBoxIndex>
+        <FLexBoxIndex inverse>
+          <div className="text_section3">
+            <h2>Code Development</h2>
+            <p>
+              Commodo reprehenderit qui sit esse magna enim sit esse dolore
+              proident eu deserunt nostrud. Officia ut id consectetur officia
+              sint minim sint sit occaecat duis ipsum sit. Ad voluptate tempor
+              nulla esse nisi. Culpa proident nulla irure reprehenderit aute
+              laboris aute.
+            </p>
+          </div>
+          <div className="image">
+            <img src="pic02.jpg" alt="pic02" />
+          </div>
+        </FLexBoxIndex>
+        <FLexBoxIndex>
+          <div className="image">
+            <img src="pic03.jpg" alt="pic03" />
+          </div>
+          <div className="text_section3">
+            <h2>Code Development</h2>
+            <p>
+              Commodo reprehenderit qui sit esse magna enim sit esse dolore
+              proident eu deserunt nostrud. Officia ut id consectetur officia
+              sint minim sint sit occaecat duis ipsum sit. Ad voluptate tempor
+              nulla esse nisi. Culpa proident nulla irure reprehenderit aute
+              laboris aute.
+            </p>
+          </div>
+        </FLexBoxIndex>
+      </SectionThree>
+      <SectionFour>
+        <div className="header_section4">
+          <div className="title_section4">
+            Technologies
+          </div>
+        </div>
+      </SectionFour>
+    </>
   )
 }
--- src/styles/indexStyles.js
+++ src/styles/indexStyles.js
@@ -1,62 +1,62 @@
-import styled from 'styled-components'
+import styled from "styled-components"
 
 const Banner = styled.div`
-    &:after {
-        content: "";
-        display: block;
-        height: 100vh;
-        width: 100%;
-        background-image: url('banner.jpg');
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center;
-        filter: blur(2px)
-    }
+  &:after {
+    content: "";
+    display: block;
+    height: 100vh;
+    width: 100%;
+    background-image: url("banner.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    filter: blur(1px);
+  }
 `
 
 const TextWrapper = styled.div`
-    position: absolute;
-    z-index: 1;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    color: white;
-    div {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-    }
-    h2 {
-        font-size: 5rem;
-        opacity: 1;
-        padding: 0.3em 1em;
-        border-top: 2px solid white;
-        border-bottom: 2px solid white;
-        text-transform: uppercase;
-        margin: 0;
-    }
-    p {
-        text-transform: uppercase;
-        text-align: center;
-        letter-spacing: 0.22em;
-        font-size: 2.5rem;
-    }
-    a {
-        background-color: #ed4933;
-        box-shadow: none;
-        color: #ffffff;
-        border-radius: 3px;
-        border: 0;
-        cursor: pointer;
-        font-size: 1.5rem;
-        font-weight: 600;
-        letter-spacing: 0.225em;
-        padding: 1.5rem 0.8em;
-        text-align: center;
-        text-decoration: none;
-        text-transform: uppercase;
-    }
+  position: absolute;
+  z-index: 1;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+  h2 {
+    font-size: 5rem;
+    opacity: 1;
+    padding: 0.3em 1em;
+    border-top: 2px solid white;
+    border-bottom: 2px solid white;
+    text-transform: uppercase;
+    margin: 0;
+  }
+  p {
+    text-transform: uppercase;
+    text-align: center;
+    letter-spacing: 0.22em;
+    font-size: 2.5rem;
+  }
+  a {
+    background-color: #ed4933;
+    box-shadow: none;
+    color: #ffffff;
+    border-radius: 3px;
+    border: 0;
+    cursor: pointer;
+    font-size: 1.5rem;
+    font-weight: 600;
+    letter-spacing: 0.225em;
+    padding: 1.5rem 0.8em;
+    text-align: center;
+    text-decoration: none;
+    text-transform: uppercase;
+  }
 `
 
 const DynamicText = styled.div`
@@ -86,4 +86,112 @@ const DynamicText = styled.div`
   }
 `
 
-export {Banner, TextWrapper, DynamicText}
+const SectionTwo = styled.section`
+  background-color: #21b2a6;
+  text-align: center;
+  padding: 10rem 0;
+  div {
+    width: 66%;
+    margin: 0 auto;
+  }
+
+  h2 {
+    font-size: 3rem;
+    padding: 2rem 0;
+    color: #ffffff;
+    border-bottom: 1.5px solid #1d9c91;
+    text-transform: uppercase;
+    letter-spacing: 0.5rem;
+    margin: 0;
+  }
+  p {
+    text-transform: uppercase;
+    color: #c8ece9;
+    text-align: center;
+    letter-spacing: 0.225em;
+    font-size: 1rem;
+  }
+  h5 {
+    font-size: 1.4em;
+    line-height: 2rem;
+    color: #ffffff;
+    border-bottom: 2px solid #1d9c91;
+    font-weight: 800;
+    letter-spacing: 0.255em;
+    text-transform: uppercase;
+    padding-bottom: 0.5rem;
+    margin-bottom: 5rem;
+  }
+`
+
+const SectionThree = styled.section`
+  background-color: #2b2b2b;
+  color: #ffff;
+`
+
+const FLexBoxIndex = styled.div`
+  display: flex;
+  .image {
+    width: ${props => (
+      props.inverse ? "60%" : "40%"
+    )};
+  }
+  img {
+    width: 100%;
+  }
+  .text_section3 {
+    width: ${props => (
+      props.inverse ? "40%" : "60%"
+    )};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+  h2 {
+    font-size: 3rem;
+    color: #ffffff;
+    text-transform: uppercase;
+    letter-spacing: 0.225rem;
+    margin: 0;
+  }
+  p {
+    text-transform: uppercase;
+    color: #c8ece9;
+    text-align: center;
+    letter-spacing: 0.075em;
+    font-size: 1.5rem;
+  }
+`
+const GenPara = styled.p`
+  text-transform: uppercase;
+  text-align: center;
+  letter-spacing: ${props => (props.lessSpacing ? "0.075em" : "0.225em")};
+  font-size: ${props => (props.lessSize ? "1.5rem" : "2.5rem")};
+  line-height: ${props => (props.lessSize ? "2rem" : "3rem")};
+  color: ${props => (props.grey ? "#c8ece9" : "#ffffff")};
+`
+const GenHead2 = styled.h2`
+  font-size: 3rem;
+  text-align: center;
+  padding: ${props => (props.none ? "0" : "1.35em 0")};
+  border-bottom: ${props => (props.none ? "0" : "2px solid #1d9c91")};
+  text-transform: uppercase;
+  letter-spacing: 0.225rem;
+  margin: 0;
+`
+const SectionFour = styled.section`
+  background-color: #21b2a6;
+  color: #ffffff;
+`
+export {
+  Banner,
+  TextWrapper,
+  DynamicText,
+  SectionTwo,
+  SectionThree,
+  FLexBoxIndex,
+  GenPara,
+    GenHead2,
+  SectionFour
+}
